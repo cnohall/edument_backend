8eaec9c18e5cae9b7c1753db11d6958bbc83c64b
Christopher <christopher.nohall@gmail.com>
2020-01-09 12:43:52 +0800

    added paths

--- routes/crud.js
+++ routes/crud.js
@@ -8,6 +8,13 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.post('/paths/:searchword',(req, res) => {
+    const searchword = req.params.searchword;
+    Path.find({path: { '$regex' : searchword, '$options' : 'i' } })
+        .then(paths => res.json(paths))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
 router.post('/add', (req, res) => { 
     const path = req.body.path;
     const newPath = new Path({
@@ -18,5 +25,31 @@ router.post('/add', (req, res) => {
         .then(() => res.json('Path added!'))
         .catch(err => res.status(400).json("Error: " + err));
 });
+
+router.post('/delete', (req, res) => { 
+    const path = req.body.path;
+    Path.findOne({path})
+    .then(response => {
+        console.log("We found this path: " + response)
+        Path.deleteOne( {path}).then(x => {
+            console.log(x);
+            return res.send("You successfully removed the path")
+          })
+          .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.post('/update', (req, res) => { 
+    // const path = req.body.path;
+    // const newPath = new Path({
+    //     path,
+    // });
+    // console.log("Update started");
+    // newPath.save()
+    //     .then(() => res.json('Path Updated!'))
+    //     .catch(err => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
 
