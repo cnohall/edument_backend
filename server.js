c7bebd0eb1ffe3bb14bb121b8e3810d7142c9d13
Christopher <christopher.nohall@gmail.com>
2020-01-16 12:02:22 +0800

    restore prevoius

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(bodyParser.urlencoded({
 // app.use(bodyParser.json());
 
 app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+
 
 const password = process.env.ATLAS_PASSWORD;
 const ATLAS_URI = "mongodb+srv://edument:" + password+ "@cluster0-07rrr.mongodb.net/test?retryWrites=true&w=majority";
