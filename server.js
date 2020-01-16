6a524e83d8c16a4fbe2f2df5a6b06ddc1c480aa0
Christopher <christopher.nohall@gmail.com>
2020-01-16 14:06:45 +0800

    added next

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', '*');
-//     if(req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 app.use(express.json());
